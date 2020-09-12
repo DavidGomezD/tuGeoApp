1c47f74759d13c0dc7d100b3081c1332f4f2f73c
DavidGomezD <davidgoomez7@gmail.com>
2020-09-11 19:48:52 -0500

    Añade la actividad informacion

--- app/src/main/java/com/davidgd/davidgd/tugeografia/MainActivity.java
+++ app/src/main/java/com/davidgd/davidgd/tugeografia/MainActivity.java
@@ -2,10 +2,13 @@ package com.davidgd.davidgd.tugeografia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -16,17 +19,21 @@ import com.google.android.gms.ads.initialization.OnInitializationCompleteListene
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bjugar, btnInformacion;
+
     private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Quita las notificaciones
+
+        //Quita las notificaciones, no mover de lugar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
 
+        //Codigo add
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
             @Override
             public void onInitializationComplete(InitializationStatus initializationStatus) {
@@ -36,6 +43,21 @@ public class MainActivity extends AppCompatActivity {
         mAdView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+        //Fin de codigo add
+
+        //Id de los botones
+        bjugar = (Button) findViewById(R.id.bjugar);
+        btnInformacion = (Button) findViewById(R.id.botonInformacion);
+
+
+        btnInformacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // iniciamos el cambio de actividad
+                Intent i = new Intent(MainActivity.this, informacion.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/davidgd/davidgd/tugeografia/informacion.java
@@ -0,0 +1,22 @@
+package com.davidgd.davidgd.tugeografia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class informacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Quita la barra de notificaciones, no mover de lugar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_informacion);
+
+    }
+}
